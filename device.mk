193e03f1e5a32811cd7ecfc195598210e85d79fb
tuxafgmur <tuxafgmur@users.noreply.github.com>
2016-02-27 02:39:27 +0100

    sip.voip.xml already included in vendor/config/common

--- device.mk
+++ device.mk
@@ -40,8 +40,7 @@ PRODUCT_PACKAGES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml
 
 # Use the non-open-source parts, if they're present
 $(call inherit-product-if-exists, vendor/samsung/espresso3g/espresso3g-vendor.mk)
